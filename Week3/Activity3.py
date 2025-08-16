6909c1ad7466eeaba2288aae1a78061fab09e7c8
Shan Peiris <shnpeiris@gmail.com>
2025-08-16 12:15:36 +1200

    update

--- Week3/Activity3.py
+++ Week3/Activity3.py
@@ -31,18 +31,29 @@ class FileHandler:
     def calculate_minimum_rating(self, column_name):
         try:
             df = pd.read_csv(self.filename)
-            min_rating = df[column_name].min()
+            min_rating = df[["Category 1", "Category 2", "Category 3", "Category 4", "Category 5"]].min()
             print(f"Minimum Rating: {min_rating}")
         except Exception as e:
             print(f"An error occurred: {e}")
+
+    def calculate_stat(self,columns):
+        df = pd.read_csv(self.filename)
+        stats = df[columns].agg(['mean', 'max', 'min'])
+        print(stats)
+
         
 def main():
     file = FileHandler('google_review_ratings.csv')
     columnName = input("Enter the column name to be used for calculations: ")
+    columns = ["Category 1", "Category 2", "Category 3", "Category 4", "Category 5"]
     file.convert_to_pd()
+    #check calues separately
     file.calculate_average_rating(columnName)
     file.Calculate_maximum_rating(columnName)
     file.calculate_minimum_rating(columnName)
+    #Get statistics for multiple columns
+    print("Calculating statistics for columns: ", columns)
+    file.calculate_stat(columns)
     
 
 if __name__ == "__main__":
