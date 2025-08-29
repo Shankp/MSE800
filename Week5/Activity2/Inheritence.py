f72472d17622191e176f928cbf300464f5b1cfe3
Shan Peiris <shnpeiris@gmail.com>
2025-08-30 09:18:22 +1200

    add

---
+++ Week5/Activity2/Inheritence.py
@@ -0,0 +1,28 @@
+class Person:
+    id:int
+    name:str
+    address:str
+    age:int
+
+class Student(Person):
+    record:str
+
+class Lecturer(Person):
+    tax_code:str
+    salary:int
+
+
+class Staff(Person):
+    tax_code:str
+    pay_rate:int   
+
+
+def main():
+    student = Student()
+    student.id=1 #ID is inherited from parent class Person
+    student.name='test1' #name is inherited from parent class Person
+    student.record ="test" #record is is within the child class
+
+if __name__== "__main__":
+    main()
+
