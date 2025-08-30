ca9d9f82fbe51dffe59b15b41716e502c70c3a0a
Shan Peiris <shnpeiris@gmail.com>
2025-08-31 11:24:03 +1200

    encap sample

---
+++ Week5/Activty6/Encap.py
@@ -0,0 +1,37 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self._age = age
+
+    def get_age(self):
+        return self.age
+
+
+class Student(Person):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+        self.name = name
+        self._age = age
+        self.__grade = "A"
+
+    def get_grade(self):
+        return self.__grade
+
+    def get_age(self):
+        return self.age
+
+
+def main():
+    student = Student("John", 25)
+    print("name :", student.name)  # student name is accessible as its public
+    print("age :", student._age)  # student age is accessible as its protected
+    print("grade :", student.get_grade())
+
+    try:
+        print("age :", student.__grade)
+    except:
+        print("student.__grade is private variable. can access directly")
+
+
+if __name__ == "__main__":
+    main()
