6258bcc8b8d56d1fc420172e94bf215a9b9719d8
Shan Peiris <shnpeiris@gmail.com>
2025-08-30 22:19:41 +1200

    update

--- Week5/Activity4/zoo.py
+++ Week5/Activity4/zoo.py
@@ -1,10 +1,18 @@
 class Color:
+    def __init__(self, color):
+        self.color = color
+
     def get_color(self):
-        return "unknown"
+        return self.color
+
 
 class TransparentColor(Color):
+    def __init__(self, color):
+        super().__init__(color)
+
     def get_color(self):
-        return "transparent"
+        return self.color
+
 
 class Animal:
     def __init__(self, name, color: Color):
@@ -14,14 +22,16 @@ class Animal:
     def get_animal_color(self):
         return self.color.get_color()
 
-def main():
-    color = Color()
-    unknown = Animal("mammal", color)
-    print(f"{unknown.name} color: {unknown.get_animal_color()}")
 
-    transparent_color = TransparentColor()
+def main():
+    color = Color("Black")
+    mammal = Animal("Mammal", color)
+    print(f"{mammal.name} color: {mammal.get_animal_color()}")
+
+    transparent_color = TransparentColor("transparent")
     fish = Animal("Goldfish", transparent_color)
     print(f"{fish.name} color: {fish.get_animal_color()}")
+
 
 if __name__ == "__main__":
     main()
