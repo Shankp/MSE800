5903dfcbc0eeb2de423a3bf0f986fd23ce45aa8f
Shan Peiris <shnpeiris@gmail.com>
2025-08-31 12:47:02 +1200

    update

--- Week5/Activity7/LibraryManagement.py
+++ Week5/Activity7/LibraryManagement.py
@@ -23,10 +23,10 @@ class Magazine(LibraryItem):
         super().__init__(title, author)
         self.title = title
         self.author = author
-        self.__issue_frequency = issue_frequency
+        self.issue_frequency = issue_frequency
 
     def get_magazine_details(self):
-        return f"\Magazine: title : {self.title}, author : {self.author}, issue frequency : {self.__issue_frequency}"
+        return f"\Magazine: title : {self.title}, author : {self.author}, issue frequency : {self.issue_frequency}"
 
 
 class Libray:
@@ -41,23 +41,35 @@ class Libray:
         self.magazine_list.append(magazine)
 
     def display_books(self):
-        for book in self.booklist:
-            print(f"\nBook: title : {book.title}, author : {book.author}")
+        return self.magazine_list
 
     def display_magazines(self):
-        for magazine in self.magazine_list:
-            print(magazine.get_magazine_details())
+        return self.magazine_list
 
 
 def main():
+    """
+    Main function to demonstrate the usage of the Library management system.
+    This function performs the following steps:
+    1. Initializes a Library instance.
+    2. Creates a Book and a Magazine object with sample data.
+    3. Adds the Book and Magazine to the Library.
+    4. Retrieves and displays the list of books and magazines in the Library, printing their details.
+    Returns:
+        None
+    """
     library = Libray()
     book = Book("city and rain", "John Foe")
     magazine = Magazine("city and rain", "John Foe", 10)
     library.add_book(book)
     library.add_Magazine(magazine)
 
-    library.display_books()
-    library.display_magazines()
+    bookList = library.display_books()
+    for book in bookList:
+        print(f"\nBook: title : {book.title}, author : {book.author}")
+    magazineList = library.display_magazines()
+    for magazine in magazineList:
+        print(f"\nMagazines: title : {magazine.title}, author : {magazine.author}, issue frequency : {magazine.issue_frequency}")
 
 
 if __name__ == "__main__":
