0b0b33c699f65c81a7624ab66d3f2994b82114f9
Shan Peiris <shnpeiris@gmail.com>
2025-09-07 11:56:23 +1200

    activity 4

--- Week6/Activity2/sort3.py
+++ Week6/Activity2/sort3.py
@@ -4,8 +4,10 @@ employees = [
     {'name': 'Alice', 'age': 30, 'salary': 80000},
     {'name': 'Bob', 'age': 25, 'salary': 50000},
     {'name': 'Charlie', 'age': 35, 'salary': 120000},
+    {'name': 'John', 'age': 25, 'salary': 120000},
 ]
 
 if __name__ == "__main__":
-    sorted_employees = sorted(employees, key= lambda x : (x['age'], x['salary']))
-    print(sorted_employees)
+    sorted_employees = sorted(employees, key= lambda x : (x['age'],x['salary']))
+    for emp in sorted_employees:
+        print(emp)
