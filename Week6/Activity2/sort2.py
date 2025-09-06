c7b17bd3b619db3888dd1113a33cd281dfd245f9
Shan Peiris <shnpeiris@gmail.com>
2025-09-07 11:46:11 +1200

    update

--- Week6/Activity2/sort2.py
+++ Week6/Activity2/sort2.py
@@ -7,10 +7,12 @@ if __name__ == "__main__":
         {"name": "Dave", "grade": "C", "age": 25},
     ]
 
+    # for each item in the list, value in the  'age' dict key is used for sorting
     sorted_students = sorted(
         students, key=lambda x: (x["age"])
-    )  # for each item in the list, 'age' dict key is used for sorting
+    )  
     print(sorted_students)
 
+    # Reverse sorting based on age
     desc_sorted_students = sorted(students, key=lambda x: (x["age"]), reverse=True)
     print(desc_sorted_students)
