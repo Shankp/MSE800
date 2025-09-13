adae2e6b5bcba83db4fab7d4b581e89a7d685c61
Shan Peiris <shnpeiris@gmail.com>
2025-09-14 11:13:59 +1200

    week 7 acitivity 3

---
+++ Week7/Activity3/checkout.py
@@ -0,0 +1,15 @@
+from Payment_gateway import PaymentGateway
+
+# Client code
+def checkout(payment_method, amount):
+    gateway = PaymentGateway()
+    processor = gateway.get().processor(payment_method)
+    return processor.process_payment(amount)
+
+
+if __name__ == "__main__":
+    print(checkout("paypal", 100))
+    print(checkout("stripe", 200))
+    print(checkout("credit_card", 300))
+
+
---
+++ Week7/Activity3/payment_processor.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+
+# Abstract base class for payment processors
+class PaymentProcessor(ABC):
+    @abstractmethod
+    def process_payment(self, amount):
+        pass
+
+# Factory class to create payment processors
+        
+class PaymentFactory:
+    # Factory method to get the appropriate payment processor
+    @staticmethod
+    def processor(method):
+        if method == "paypal":
+            return PayPalPayment()
+        elif method == "stripe":
+            return StripePayment()
+        elif method == "credit_card":
+            return CreditCardPayment()
+        else:
+            raise ValueError("Unknown payment method")
+        
+# Concrete payment processor classes
+class PayPalPayment(PaymentProcessor):
+    def process_payment(self, amount):
+        return f"Processing ${amount} via PayPal"
+    
+# Concrete payment processor classes
+class StripePayment(PaymentProcessor):
+    def process_payment(self, amount):
+        return f"Processing ${amount} via Stripe"
+    
+# Concrete payment processor classes
+class CreditCardPayment(PaymentProcessor):
+    def process_payment(self, amount):
+        return f"Processing ${amount} via Credit Card"
+
+
