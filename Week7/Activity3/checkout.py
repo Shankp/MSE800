e36110f4fc0b8431010b7b4d9b9bbae0c4e4e887
Shan Peiris <shnpeiris@gmail.com>
2025-09-14 11:33:15 +1200

    update

--- Week7/Activity3/checkout.py
+++ Week7/Activity3/checkout.py
@@ -1,16 +1,9 @@
 from Payment_gateway import PaymentGateway
 
-# Client code
-def checkout(payment_method, amount):
-    gateway = PaymentGateway()
-    processor = gateway.get_payment_processor(payment_method)
-    return processor.process_payment(amount)
-
 
 if __name__ == "__main__":
 
     gateway = PaymentGateway()
-
 
     processor = gateway.get_payment_processor("paypal")
     paypal_payment = processor.process_payment(100)
@@ -23,6 +16,3 @@ if __name__ == "__main__":
     processor = gateway.get_payment_processor("credit_card")
     credit_card_payment = processor.process_payment(300)
     print(credit_card_payment)
-   
-
-
