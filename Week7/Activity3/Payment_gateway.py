9ecafadfb9d43eb9c4d70f3d58e27615c7c9e1b8
Shan Peiris <shnpeiris@gmail.com>
2025-09-14 11:22:26 +1200

    update

--- Week7/Activity3/Payment_gateway.py
+++ Week7/Activity3/Payment_gateway.py
@@ -1,19 +1,17 @@
 import payment_processor
-import threading
+
 
 # Singleton class for Payment Gateway
 class PaymentGateway:
     _instance = None
-    _lock = threading.Lock()
 
     def __new__(cls):
         if cls._instance is None:
-            with cls._lock:
-                if cls._instance is None:
-                    cls._instance = super(PaymentGateway, cls).__new__(cls)
-                    cls._instance.gateway = None
+            cls._instance = super(PaymentGateway, cls).__new__(cls)
+            cls._instance.gateway = None
         return cls._instance
 
+    # Method to get the payment factory instance
     def get(self):
         if self.gateway is None:
             self.gateway = payment_processor.PaymentFactory()
--- Week7/Activity3/payment_processor.py
+++ Week7/Activity3/payment_processor.py
@@ -1,15 +1,15 @@
 from abc import ABC, abstractmethod
 
+
 # Abstract base class for payment processors
 class PaymentProcessor(ABC):
     @abstractmethod
     def process_payment(self, amount):
         pass
 
+
 # Factory class to create payment processors
-        
 class PaymentFactory:
-    # Factory method to get the appropriate payment processor
     @staticmethod
     def processor(method):
         if method == "paypal":
@@ -20,20 +20,21 @@ class PaymentFactory:
             return CreditCardPayment()
         else:
             raise ValueError("Unknown payment method")
-        
+
+
 # Concrete payment processor classes
 class PayPalPayment(PaymentProcessor):
     def process_payment(self, amount):
         return f"Processing ${amount} via PayPal"
-    
+
+
 # Concrete payment processor classes
 class StripePayment(PaymentProcessor):
     def process_payment(self, amount):
         return f"Processing ${amount} via Stripe"
-    
+
+
 # Concrete payment processor classes
 class CreditCardPayment(PaymentProcessor):
     def process_payment(self, amount):
         return f"Processing ${amount} via Credit Card"
-
-
