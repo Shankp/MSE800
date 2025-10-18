0bb492ad6a716ee3a39a8c5cbf52d6b9059493c4
Shan Peiris <shnpeiris@gmail.com>
2025-10-19 11:42:12 +1300

    update

--- week12/Activity5/basic/views.py
+++ week12/Activity5/basic/views.py
@@ -5,4 +5,4 @@ def index(request):
     return HttpResponse(f"<h1>Welcome  to Django!</h1>")
 
 def welcome(request, name):
-    return HttpResponse(f"<h1>Welcome {name} to Django!</h1>")
+    return HttpResponse(f"""<html><head><style>body{{background-color:green;color:red;font-family:Arial, sans-serif;}}</style></head><body><h1>Welcome {name} to Django!</h1></body></html>""")
