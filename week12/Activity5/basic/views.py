5fab96af432354258833d0737c64977c7691c093
Shan Peiris <shnpeiris@gmail.com>
2025-10-19 11:42:57 +1300

    update

--- week12/Activity5/basic/views.py
+++ week12/Activity5/basic/views.py
@@ -4,5 +4,17 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse(f"<h1>Welcome  to Django!</h1>")
 
+
 def welcome(request, name):
-    return HttpResponse(f"""<html><head><style>body{{background-color:green;color:red;font-family:Arial, sans-serif;}}</style></head><body><h1>Welcome {name} to Django!</h1></body></html>""")
+    return HttpResponse(
+        f"""
+                        <html>
+                        <head>
+                        <style>
+                        body{{background-color:green;color:red;font-family:Arial, sans-serif;}}</style>
+                        </head>
+                        <body>
+                        <h1>Welcome {name} to Django!</h1>
+                        </body>
+                        </html>"""
+    )
